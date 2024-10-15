6316dbbbacc4f975e3ac69e5ef5f299d5ebeb20c
syukri124 <syukriihsan680@gmail.com>
2024-10-15 18:49:35 +0700

    Add files via upload

---
+++ Playlist Lagu/src/playlist/lagu/PlaylistLagu.java
@@ -0,0 +1,59 @@
+package playlist.lagu;
+
+class Song {
+    String title;
+    Song next;
+
+    // Konstruktor
+    public Song(String title) {
+        this.title = title;
+        this.next = null;
+    }
+}
+
+public class PlaylistLagu {
+    private Song head;
+
+    public PlaylistLagu() {
+        this.head = null;
+    }
+
+    public void addSong(String title) {
+        Song newSong = new Song(title);
+
+        if (head == null) {
+            head = newSong;
+        } else {
+            Song current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newSong;
+        }
+    }
+
+    public void displayPlaylist() {
+        if (head == null) {
+            System.out.println("Playlist kosong.");
+            return;
+        }
+
+        Song current = head;
+        while (current != null) {
+            System.out.println(current.title);
+            current = current.next;
+        }
+    }
+
+    public static void main(String[] args) {
+        PlaylistLagu myPlaylist = new PlaylistLagu();
+
+        myPlaylist.addSong("Song 1");
+        myPlaylist.addSong("Song 2");
+        myPlaylist.addSong("Song 3");
+
+        System.out.println("Playlist:");
+        myPlaylist.displayPlaylist();
+    }
+}
+
